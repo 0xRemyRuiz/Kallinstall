8410fbdf874ac8e100eb31a430578e941feab4cb
0xRemyRuiz <remyruiz@protonmail.com>
2025-02-21 14:06:31 +0000

    added correct secure permissions on step holder file

--- kalinstall.sh
+++ kalinstall.sh
@@ -43,11 +43,12 @@ set_step() {
 	STEP=$(2powStep $1)
 	# add the step mask with an OR bitwise operation
 	_STEPS=$(( $_STEPS | $STEP ))
-	# we rewrite the file everytime it is needed no necessity to optimize this but it's needed
-	# TODO: sudo only remove and rewrite the isntalls steps file
-	rm -f $_STEPS_FILENAME
-	echo "_STEPS=$_STEPS" > $_STEPS_FILENAME
-	echo "export _STEPS" >> $_STEPS_FILENAME
+	# we rewrite the file everytime it is needed there is no need to optimize this
+	rm $_STEPS_FILENAME &&\
+	echo "_STEPS=$_STEPS" > $_STEPS_FILENAME &&\
+	echo "export _STEPS" >> $_STEPS_FILENAME &&\
+	chown root:root $_STEPS_FILENAME &&\
+	chmod 600 $_STEPS_FILENAME
 }
 check_step() {
 	STEP=$(2powStep $1)
@@ -60,13 +61,25 @@ check_step() {
 	fi	
 }
 
+
+####
+# Start script
+####
+if [ $(id -u) -ne 0 ]
+then
+	echo "ERROR: script must be run as root"
+	exit 1
+fi
+
+# TODO: add a listing of desired steps to do
+
 # TODO: make a check presence of the install steps file and display a message
 
 # load steps already done if any
 # the file is loaded and written as root to avoid basic injection problems linked to the usage of source command
 # so it would not be possible to write to the file with any basic user privilege
 # also, the file would not be readable from anyone but the root as it could leak config info
-# TODO: add sudo -s if file exists
+# TODO: add -s if file exists
 source $_STEPS_FILENAME
 
 # TODO: change message is install step file exists already or not
@@ -82,22 +95,22 @@ check_step 1 && step1 && set_step 1
 
 step2() {
 	my_echo "Making sure sources are correct"
-	sudo sed -i 's/rolling main.*$/rolling main contrib non-free non-free-firmware/' /etc/apt/sources.list
+	sed -i 's/rolling main.*$/rolling main contrib non-free non-free-firmware/' /etc/apt/sources.list
 	# try to update or exit the script
-	sudo apt-get update || (my_echo "apt-get update failed, make sure you have internet conenction "; exit)
-	sudo apt-get install apt-transport-https && sudo sed -i 's/http:/https:/' /etc/apt/sources.list
+	apt-get update || (my_echo "apt-get update failed, make sure you have internet conenction "; exit)
+	apt-get install apt-transport-https && sed -i 's/http:/https:/' /etc/apt/sources.list
 }
 check_step 2 && step2 && set_step 2
 
 step3() {
 	check "Do you need to change your keyboard layout?" &&\
-	sudo dpkg-reconfigure keyboard-configuration
+	dpkg-reconfigure keyboard-configuration
 }
 check_step 3 && step3 && set_step 3
 
 step4() {
 	my_echo "Updating distro and installing basic stuff..."
-	sudo apt-get full-upgrade && sudo apt-get install git gpg apt-transport-https xclip
+	apt-get full-upgrade && apt-get install git gpg apt-transport-https xclip
 }
 check_step 4 && step4 && set_step 4
 
@@ -105,7 +118,7 @@ check_step 4 && step4 && set_step 4
 
 step5() {
 	my_echo "Installing python3 + venv + pip + pipx"
-	sudo apt-get install python3-dev python3-pip python3-virtualenv python3-venv python3-scapy libssl-dev libpcap-dev
+	apt-get install python3-dev python3-pip python3-virtualenv python3-venv python3-scapy libssl-dev libpcap-dev
 	updatedb
 	pip install pipx && set_step 5
 }
@@ -113,7 +126,7 @@ check_step 5 && step5
 
 step6() {
 	my_echo "Installing python2.7 + pip"
-	sudo apt-get install python2.7
+	apt-get install python2.7
 	wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
 	python2 get-pip.py && set_step 6
 }
@@ -128,52 +141,52 @@ exit
 # TODO: install oh my zsh and eventually kali's omz if using this script on parrot os
 
 subl_install() {
-	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-	sudo apt-get update -y && sudo apt-get install sublime-text -y && set_step 7
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+	echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+	apt-get update -y && apt-get install sublime-text -y && set_step 7
 }
-check_step 7 && check "Do you want to install SublimeText?" &&\
+check_step 8 && check "Do you want to install SublimeText?" &&\
 subl_install
 
 chkrk_install() {
-	sudo apt-get install chkrootkit -y
+	apt-get install chkrootkit -y
 	my_echo "Running chkrootkit. Wait!"
-	sudo chkrootkit > ./chkrootkit_log.txt && set_step 8
+	chkrootkit > ./chkrootkit_log.txt && set_step 8
 }
-check_step 8 && check "Do you want to install chkrootkit?" &&\
+check_step 9 && check "Do you want to install chkrootkit?" &&\
 chkrk_install
 
 rkhunter_install() {
-	sudo apt-get install rkhunter -y
+	apt-get install rkhunter -y
 	rkhunter --update
-	rkhunter -c && set_step 9
+	rkhunter -c && set_step 10
 }
-check_step 9 && check "Do you want to install rkhunter?" &&\
+check_step 10 && check "Do you want to install rkhunter?" &&\
 rkhunter_install
 
 lynis_install() {
-	sudo apt-get install lynis -y
+	apt-get install lynis -y
 	my_echo "Running Lynis, please wait..."
-	lynis audit system > ./.lynis_log.txt && set_step 10
+	lynis audit system > ./.lynis_log.txt && set_step 11
 }
-check_step 10 && check "Do you want to install lynis?" &&\
+check_step 11 && check "Do you want to install lynis?" &&\
 lynis_install
 
 opencanary_install() {
 	virtualenv env/ #?? ??
 	. env/bin/activate
-	pip install opencanary && set_step 11
+	pip install opencanary && set_step 12
 }
-# check_step 11 && check "Do you want to install opencanary?" &&\
+# check_step 12 && check "Do you want to install opencanary?" &&\
 # opencanary_install
 
 updatedb
 
 # TODO: install htool and update path variables (both user and root?)
 Htoolkit_install() {
-	set_step 12
+	set_step 13
 }
-# check_step 12 && check "Do you want to install Htoolkit?" &&\
+# check_step 13 && check "Do you want to install Htoolkit?" &&\
 # Htoolkit_install
 
 # TODO: remove the install steps file
